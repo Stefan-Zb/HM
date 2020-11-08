511458d84eb371b7ee6c34ee74cac7e26e169d0f
Stefan-Zb <72871477+Stefan-Zb@users.noreply.github.com>
2020-11-08 21:09:55 +0200

    08.11.2020 ws

---
+++ Homework/JavaScript/homework/homework-js-4/javas4.js
@@ -0,0 +1,37 @@
+// // - створити функцію яка виводить масив
+let arr1 = [1, 2, 3, 4, 5, 6, 7];
+function showArray(arr){
+    console.log(arr);
+}
+showArray(arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

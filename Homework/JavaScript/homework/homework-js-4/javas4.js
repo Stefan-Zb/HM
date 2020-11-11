079f500be85273c4bd95820352a0d68315a84805
Stefan-Zb <72871477+Stefan-Zb@users.noreply.github.com>
2020-11-11 07:27:12 +0200

    11.11.2020

--- Homework/JavaScript/homework/homework-js-4/javas4.js
+++ Homework/JavaScript/homework/homework-js-4/javas4.js
@@ -1,12 +1,51 @@
-// // - створити функцію яка виводить масив
+        // // - створити функцію яка виводить масив
+
 let arr1 = [1, 2, 3, 4, 5, 6, 7];
 function showArray(arr){
     console.log(arr);
 }
 showArray(arr1);
 
+        // - створити функцію яка заповнює масив рандомними числами та виводить його. Для виведення використати попвередню функцію.
 
+let randomArr = [];
+function Random(arr) {
+    for (let i = 0; i < 10; i++){
+        arr.push(Math.round(Math.random()*100));
+    }
+    showArray(arr);
+}
+Random(randomArr);
 
+        // // - створити функцію яка приймає три числа та виводить та повертає найменьше.
+
+function Min(one, two, three){
+    let result = Math.min(one, two, three);
+    console.log(result);
+    return result
+}
+Min(60, 8, 25);
+
+        // - створити функцію яка приймає три числа та виводить та повертає найбільше.
+
+function Max(one, two, three){
+    let result = Math.max(one, two, three,);
+    console.log(result);
+    return result
+}
+Max(10, 5, 15);
+
+        // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
+
+function MinMax(){
+    let args = [];
+    for (let i = 0; i < arguments.length; i++){
+        args.push(arguments[i]);
+    }
+    console.log(Math.max(...args));
+    return Math.min(...args);
+}
+console.log(MinMax(10, 2, 3, 4, 5, 6, 7, 10, 50, 15, 30));
 
 
 
---
+++ Homework/JavaScript/homework/homework-js-6/javas6.js
@@ -0,0 +1,120 @@
+//         // - создать массив с 20 числами.;
+//
+// let masyv = [2, 8, 3, 22, 45, 1, 50, 23, 41, 18, 62, 32, 80, 41, 33, 21, 14, 35, 24];
+//
+//         // -- при помощи метода sort и колбека  отсортировать массив.
+//
+// let sort = masyv.sort((a,b) => {
+//   return a-b;
+// })
+// console.log(sort);
+//
+//         // -- при помощи метода sort и колбека отсортировать массив в ниспадающем напралении.
+//
+// let sort2 = masyv.sort((a,b)=>{
+//   return b-a;
+// })
+// console.log(sort2);
+//
+//         // -- при помощи filter получить числа кратные 3
+//
+// let filter1 = masyv.filter((value) => !(value % 3));
+// console.log(filter1);
+//
+//         // -- при помощи filter получить числа кратные 10
+//
+// let filter2 = masyv.filter((value) => !(value % 10));
+// console.log(filter2);
+//
+//         // -- перебрать (проитерировать) массив при помощи foreach()
+//
+// masyv.forEach((value, index)=> console.log(index, value));
+//
+//         // -- перебрать массив при помощи map() и получить новый массив в котором все значения будут в 3 раза больше
+//
+// masyv.map((value)=>console.log(value * 3));
+
+        // - создать массив со словами на 15-20 элементов.
+
+// let masyv2 = ['создать', 'массив', 'со', 'словами', 'перебрать', 'массив', 'и', 'получить', 'новый', 'массив', 'перебрать', 'проитерировать', 'массив'];
+//
+//         // -- отсортировать его по алфавиту в восходящем порядке.
+//
+// let sort3 = masyv2.sort((a,b)=> {
+//   if (a>b) {
+//     return 1;
+//   }
+//   return  -1;
+// })
+// console.log(sort3);
+//
+//         // -- отсортировать его по алфавиту  в нисходящем порядке.
+//
+// let sort4 = masyv2.sort((a,b)=>{
+//   if (a>b) {
+//     return -1;
+//   }
+//   return  1;
+// })
+// console.log(sort4);
+//
+//       // -- отфильтровать слова длиной менее 4х символов
+//
+// console.log(masyv2.filter((value) => (value.length < 4)));
+//
+//       // -- перебрать массив при помощи map() и получить новый массив в котором все значения будут со знаком "!" в конце
+//
+// let mapa = masyv2.map((value) => (value + '!'));
+// console.log(mapa);
+//
+//       // Все робити через функції масивів (foreach, map ...тд)
+//       // Дан масив :
+//   let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+//     ];
+//
+//         // - відсортувати його за  віком (зростання , а потім окремо спадання)
+//
+// console.log(users.sort((a,b) => a.age-b.age));
+//
+// console.log(users.sort((a,b) => b.age-a.age));
+//
+//         // - відсортувати його за кількістю знаків в імені  (зростання , а потім окремо спадання)
+//
+// console.log(users.sort((a, b) => a.name.length-b.name.length));
+//
+// console.log(users.sort((a, b) => b.name.length-a.name.length));
+//
+//         // - пройтись по ньому та додати кожному юзеру поле id - яке характеризує унікальний індентифікатор (По якому принципу його створювати - ваше рішення), та зберегти це в новий масив (первинний масив залишиться без змін)
+//
+// let xxx = [];
+// for (let user of users){
+//   let a = JSON.stringify(user);
+//   xxx.push(a);
+// }
+// let newmasyv = [];
+// for (let user of xxx) {
+//   let a = JSON.parse(user);
+//   newmasyv.push(a);
+// }
+// newmasyv.map((value,index)=>{
+//     return value['id']=index;
+//     }
+// );
+// console.log(newmasyv);
+//
+//         // - відсортувати його за індентифікатором
+//
+// console.log(newmasyv.sort((a, b) => b.id-a.id));
+
+
